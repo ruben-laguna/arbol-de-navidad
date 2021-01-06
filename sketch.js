532947c07e5f489f8db3be11d9ead86d244ccbe1
Ruben L <a01652362@itesm.mx>
2021-01-06 13:08:30 -0600

    Git Hub

---
+++ sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(700, 500);
+  background(242, 243, 244);
+  fill("green")
+  stroke("green")
+  triangle(250,150, 450, 150, 350, 35);
+  triangle(250,200, 450, 200, 350, 70);
+  triangle(250,250, 450, 250, 350, 135);
+  triangle(250,300, 450, 300, 350, 200);
+   fill("brown")
+  square(325, 300, 55)
+
+}
+
+function draw() {
+  fill(random(0,255), 0, random(0,255));
+  strokeWeight();
+    ellipse(320,178, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(380,160, 15);
+   fill(random(0,255), 0, random(0,255));
+    ellipse(320,95, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(365,100, 15);
+   fill(random(0,255), 0, random(0,255));
+    ellipse(280,140, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(360,220, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(360,220, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(290,230, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(410,235, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(370,285, 15);
+  fill(random(0,255), 0, random(0,255));
+    ellipse(320,270, 15);
+  
+}
